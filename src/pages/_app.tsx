fe2f8607d37ca29593ecfd08a0d60746c2cd6f0e
Vladislav Yakovlev <vlad309523@gmail.com>
2020-09-23 11:25:50 +0300

    Disallow robots

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ const App = memo(function App(props: AppProps) {
     <Head>
       <title>Свадьба Яковлевых</title>
       <meta name="viewport" content="initial-scale=1, viewport-fit=cover"/>
+      <meta name="robots" content="noindex" />
     </Head>
 
     <Layout>{createElement(props.Component, props.pageProps)}</Layout>
